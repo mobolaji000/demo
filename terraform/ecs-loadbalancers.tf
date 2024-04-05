4643c8b14ffdc9508759fbf0455b6e75940b65e2
Mo Akinpelu <mobolajiakinpelu00@gmail.com>
2024-04-05 12:51:59 -0500

    created server

--- terraform/ecs-loadbalancers.tf
+++ terraform/ecs-loadbalancers.tf
@@ -1,5 +1,5 @@
-resource "aws_alb" "client_alb" {
-  name_prefix = "srv-"
+resource "aws_lb" "client_alb" {
+  name_prefix = "cl-"
   load_balancer_type = "application"
   security_groups = [aws_security_group.client_alb.id]
   subnets = aws_subnet.public.*.id
@@ -11,8 +11,8 @@ resource "aws_alb" "client_alb" {
   }
 }
 
-resource "aws_alb_target_group" "client_alb_targets" {
-  name_prefix = "srv-"
+resource "aws_lb_target_group" "client_alb_targets" {
+  name_prefix = "cl-"
   port = 9090
   protocol = "HTTP"
   vpc_id = aws_vpc.main.id
@@ -36,14 +36,63 @@ resource "aws_alb_target_group" "client_alb_targets" {
 
 }
 
-
-resource "aws_alb_listener" "client_alb_http_80" {
-  load_balancer_arn = aws_alb.client_alb.arn
+resource "aws_lb_listener" "client_alb_http_80" {
+  load_balancer_arn = aws_lb.client_alb.arn
   port = 80
   protocol = "HTTP"
 
   default_action {
     type = "forward"
-    target_group_arn = aws_alb_target_group.client_alb_targets.arn
+    target_group_arn = aws_lb_target_group.client_alb_targets.arn
+  }
+}
+
+########################################
+
+resource "aws_lb" "server_alb" {
+  name_prefix = "srv-"
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.server_alb.id]
+  subnets = aws_subnet.private.*.id
+  idle_timeout = 60
+  internal = true
+  tags = {
+    Name = "${var.default_tags.project}-server-alb"
+  }
+}
+
+resource "aws_lb_target_group" "server_alb_targets" {
+  name_prefix = "srv-"
+  port = 9090
+  protocol = "HTTP"
+  vpc_id = aws_vpc.main.id
+  deregistration_delay = 30
+  target_type = "ip"
+
+  health_check {
+    enabled = true
+    path = "/"
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    timeout = 29
+    protocol = "HTTP"
+
+  }
+
+  tags = {
+    Name = "${var.default_tags.project}-server-tg"
+  }
+
+
+}
+
+resource "aws_lb_listener" "server_alb_http_80" {
+  load_balancer_arn = aws_lb.server_alb.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.server_alb_targets.arn
   }
 }
--- terraform/ecs-services.tf
+++ terraform/ecs-services.tf
@@ -6,7 +6,7 @@ resource "aws_ecs_service" "client" {
   launch_type = "FARGATE"
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.client_alb_targets.arn
+    target_group_arn = aws_lb_target_group.client_alb_targets.arn
     container_name = "client"
     container_port = 9090
   }
@@ -15,5 +15,25 @@ resource "aws_ecs_service" "client" {
     subnets = aws_subnet.private.*.id
     assign_public_ip = false
     security_groups = [aws_security_group.ecs_client_service.id]
+  }
+}
+
+resource "aws_ecs_service" "server" {
+  name = "${var.default_tags.project}-server"
+  cluster = aws_ecs_cluster.main.arn
+  task_definition = aws_ecs_task_definition.server.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.server_alb_targets.arn
+    container_name = "server"
+    container_port = 9090
+  }
+
+  network_configuration {
+    subnets = aws_subnet.private.*.id
+    assign_public_ip = false
+    security_groups = [aws_security_group.ecs_server_service.id]
   }
 }
